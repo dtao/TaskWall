43e54d9d04453c69583de70ce818af99bfa486f5
Dan Tao <dan@cardpool.com>
2012-08-10 11:20:27 -0700

    updated database.rb with correct environment variable name for Heroku

--- config/database.rb
+++ config/database.rb
@@ -2,7 +2,7 @@ DataMapper.logger = logger
 DataMapper::Property::String.length(255)
 
 case Padrino.env
-  when :production  then DataMapper.setup(:default, ENV["DATABASE_URL"])
+  when :production  then DataMapper.setup(:default, ENV["HEROKU_POSTGRESQL_GOLD_URL"])
   when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "unfuddle_metrics_development.db"))
   when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "unfuddle_metrics_test.db"))
 end
